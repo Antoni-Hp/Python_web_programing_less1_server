733054d82163211537514b0be1fe379b2352b8a7
Henryk Panas <henrykantoni@hotmail.com>
2018-10-30 10:32:52 +0100

    Server

--- Web_pr_less1_server/Web_pr_less1_server.py
+++ Web_pr_less1_server/Web_pr_less1_server.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+import socket
+con_adres = ('0.0.0.0', 6500)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(con_adres)
+sock.listen(1)
+conn, ca = sock.accept()
+data = connection.recv(1024)
+conn.send("DUPA")
+conn.close()
